06a637696646178142fe547e99edeeca6725d52c
momin096 <momin09609@gmail.com>
2025-05-21 18:43:28 +0600

    ssl commarz payment added

--- index.js
+++ index.js
@@ -6,14 +6,50 @@ var jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { default: axios } = require('axios');
+
+// const formData = require('form-data');
+// const Mailgun = require('mailgun.js');
+
+// const mailgun = new Mailgun(formData);
+
+
+// const mg = mailgun.client({
+//     username: 'api',
+//     key: process.env.MAIL_GUN_API_KEY, // e.g., 'key-1234567890abcdef'
+// });
+
+// module.exports = mg;
+
+
+// SSL COMMARZZ ---------------------------------------------------
+// Store ID: bistr682c0082a8cfd
+// Store Password(API / Secret Key): bistr682c0082a8cfd@ssl
+
+
+// Merchant Panel URL: https://sandbox.sslcommerz.com/manage/ (Credential as you inputted in the time of registration)
 
 
 
+// Store name: testbistr5a39
+// Registered URL: www.bistroboss.com
+// Session API to generate transaction: https://sandbox.sslcommerz.com/gwprocess/v3/api.php
+// Validation API: https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php?wsdl
+// Validation API(Web Service) name: https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php
+
+// You may check our plugins available for multiple carts and libraries: https://github.com/sslcommerz
+
+// ssl commarz steps
+/**
+ * 1. initiate payment 
+ * 2. 
+ */
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tp3bo.mongodb.net/?appName=Cluster0`;
@@ -28,7 +64,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();  
 
         const menuCollection = client.db('bistroDB').collection('menu');
         const reviewCollection = client.db('bistroDB').collection('reviews');
@@ -65,8 +101,6 @@ async function run() {
                 next();
             })
         }
-
-
         const verifyAdmin = async (req, res, next) => {
             const email = req?.decoded?.email;
             const query = { email };
@@ -80,8 +114,6 @@ async function run() {
 
             next();
         }
-
-
         // users collection
 
         app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
@@ -110,8 +142,6 @@ async function run() {
 
         })
 
-
-
         app.delete('/users/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -130,7 +160,6 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
-
 
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -244,6 +273,8 @@ async function run() {
             })
         })
 
+
+
         // payment related api
         app.post('/payments', async (req, res) => {
             const payment = req.body;
@@ -254,6 +285,22 @@ async function run() {
                     $in: payment.cartIds.map(id => new ObjectId(id))
                 }
             };
+            // not verified
+            // mg.messages.create(process.env.MAIL_SENDING_DOMAIN, {
+            //     from: 'Mailgun Sandbox <postmaster@sandbox5c3501ff6b46453eba921485e5fe35ed.mailgun.org>',
+            //     to: ['momin09609@gmail.com'],
+            //     subject: 'Bistro Boss Order Confirmation',
+            //     text: 'Testing MailGun Awesomeness!',
+            //     html: `
+            //     <div>
+            //         <h2>Thank You for your order</h2>
+            //         <h4>Your Transaction id: <strong>${payment.transactionId}</h4>
+            //         <P>We would like to get your feedback about the food</p>
+            //     </div>
+            //     `
+            // })
+            //     .then(msg => console.log(msg)) // success
+            //     .catch(err => console.error(err)); // error
 
             const deleteResult = await cartCollection.deleteMany(query);
             res.send({ paymentResult, deleteResult })
@@ -269,9 +316,104 @@ async function run() {
             const result = await paymentCollection.find(query).toArray();
             res.send(result);
         })
+        // ssl payment initiate 
+        app.post('/create-ssl-payment', async (req, res) => {
+            const payment = req.body
+            // console.log('payment Info', payment);
+
+            const trxid = new ObjectId().toString()
+
+            payment.transactionId = trxid
+
+            const initiate = {
+                store_id: 'bistr682c0082a8cfd',
+                store_passwd: 'bistr682c0082a8cfd@ssl',
+                total_amount: payment.price,
+                currency: 'BDT',
+                tran_id: trxid, // use unique tran_id for each api call
+                success_url: 'http://localhost:5000/success-payment',
+                fail_url: 'http://localhost:5173/fail',
+                cancel_url: 'http://localhost:5173/cancel',
+                ipn_url: 'http://localhost:5000/ipn-success-payment',
+                shipping_method: 'Courier',
+                product_name: 'Computer.',
+                product_category: 'Electronic',
+                product_profile: 'general',
+                cus_name: 'Customer Name',
+                cus_email: `${payment?.email}`,
+                cus_add1: 'Dhaka',
+                cus_add2: 'Dhaka',
+                cus_city: 'Dhaka',
+                cus_state: 'Dhaka',
+                cus_postcode: '1000',
+                cus_country: 'Bangladesh',
+                cus_phone: '01711111111',
+                cus_fax: '01711111111',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: 1000,
+                ship_country: 'Bangladesh',
+            }
+
+            const iniResponse = await axios({
+                url: 'https://sandbox.sslcommerz.com/gwprocess/v4/api.php',
+                method: 'POST',
+                data: initiate,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+            })
+
+            const saveData = await paymentCollection.insertOne(payment)
+            const gatewayUrl = iniResponse?.data?.GatewayPageURL
+
+            console.log('gatewayUrl', gatewayUrl, saveData);
+            res.send({ gatewayUrl })
 
 
-        // stats or analytics 
+        })
+
+        // payment success 
+        app.post('/success-payment', async (req, res) => {
+            // success payment data
+            const paymentSuccess = req.body
+            // console.log('payment success info', paymentSuccess);
+
+            // validation
+            const { data } = await axios.get(`https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php?val_id=${paymentSuccess.val_id}&store_id=bistr682c0082a8cfd&store_passwd=bistr682c0082a8cfd@ssl&format=json`)
+
+            if (data.status !== 'VALID') {
+                res.send({ message: 'Invalid Payment' })
+            }
+
+            // update the payment
+            const updatePayment = await paymentCollection.updateOne({ transactionId: data.tran_id }, {
+                $set: { status: 'success' }
+            })
+
+            console.log(updatePayment);
+
+            const payment = await paymentCollection.findOne({ transactionId: data.tran_id })
+            console.log(payment);
+
+            query = {
+                _id: {
+                    $in: payment.cartIds.map(id => new ObjectId(id))
+                }
+            };
+
+            const deleteResult = await cartCollection.deleteMany(query);
+            console.log(deleteResult, 'deleteresult');
+            res.redirect('http://localhost:5173/success')
+
+
+        })
+
+
+        // stats or analytics  -------------------------------------
         app.get('/admin-stats', verifyJWT, verifyAdmin, async (req, res) => {
             const users = await userCollection.estimatedDocumentCount();
             const menuItems = await menuCollection.estimatedDocumentCount();
@@ -336,8 +478,8 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
